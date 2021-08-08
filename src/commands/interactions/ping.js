e05344adeef2cc9ae250f9dbb95e5592239c88d6
Sprintermax <ericfnoe7@hotmail.com>
2021-08-07 21:47:14 -0300

    Update ping.js

--- src/commands/interactions/ping.js
+++ src/commands/interactions/ping.js
@@ -5,20 +5,18 @@ module.exports = {
 		description: 'Verifica a latência do Bot e do servidor'
 	},
 	async run({ DiscordJS, Client, Interaction }) {
-		const PastMiliseconds = Date.now() - Interaction.createdTimestamp;
 		await Interaction.reply({
 			embeds: [
 				new DiscordJS.MessageEmbed()
 					.setTitle('🏓 Pong!')
 					.setDescription('Aqui está o tempo de resposta do Bot:')
 					.setColor('#000000')
-					.addField('Latência do Bot:', Math.floor(PastMiliseconds) + 'ms', true)
+					.addField('Latência do Bot:', Math.floor(Date.now() - Interaction.createdTimestamp) + 'ms', true)
 					.addField('\u200b', '\u200b', true)
 					.addField('Latência da API:', Math.round(Client.ws.ping) + 'ms', true)
 					.setFooter(`${Interaction.guild?.name || Client.user.username}`, Interaction.guild?.iconURL() || Client.user.avatarURL() || '')
 					.setTimestamp()
 			]
 		});
-
 	}
 }
